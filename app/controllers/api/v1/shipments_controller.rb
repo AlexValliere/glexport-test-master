ab66e74492b491e71ab61485d8958da6451797e4
Alexandre PETIT <alexandre.petit.vilapon@gmail.com>
2018-12-15 01:58:10 +0100

    Added more successfull tests

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -1,9 +1,30 @@
 module Api
   module V1
     class ShipmentsController < ApplicationController
+
+
       def index
-        @shipments = Shipment.all
+        if (params[:company_id].blank?)
+          @shipments = Shipment.all.includes(:shipment_products)
+        else
+          @shipments = Shipment.where(:company_id => params[:company_id]).includes(:shipment_products)
+        end
+
+        if (params[:sort].present?)
+          if (params[:direction].present?)
+            @shipments = @shipments.reorder("shipments." + params[:sort] + " " + params[:direction])
+          else
+            @shipments = @shipments.reorder("shipments." + params[:sort] + " asc")
+          end
+        end
+
+        if (params[:international_transportation_mode].present?)
+          @shipments = @shipments.select { |shipment| shipment.international_transportation_mode == params[:international_transportation_mode] }
+        end
+
       end
+
+
     end
   end
 end
